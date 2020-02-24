93baa6c9f8ce9d8d30750cbb468f0df090ab5228
Lee Peters <lpeters0023@gmail.com>
2020-02-23 21:34:48 -0500

    player name input done

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 let winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 5, 8], [0, 4, 8], [6, 4, 2]];
 let xLog = [];
 let oLog = [];
+let moveCount = []; 
 let gameStatus = document.getElementById('game-status');
 let startButton = document.getElementById('start');
+let addPlayerOButton = document.getElementById('player-o-button')
+let addPlayerXButton = document.getElementById('player-x-button')
+let playerNameO = document.getElementById('player-o-name')
+let playerNameX = document.getElementById('player-x-name')
 let playerO = false;
-let moveCount = [];
-let count = 0;
+let count = 0; // for game timer
+let singlePlayer = null; 
+let singlePlayerButton = document.getElementById('single-player')
+let multiPlayerButton = document.getElementById('multi-player')
+let playerNameInput = document.getElementById('add-player-input')
+    playerNameInput.style.visibility = "hidden"
+    
+// | - - - add player names - - - |
+singlePlayerButton.addEventListener('click', () => {
+  multiPlayerButton.disabled = true;
+  //random num generator
+})
+multiPlayerButton.addEventListener('click', () => {
+  singlePlayerButton.disabled = true;
+  playerNameInput.style.visibility = "visible"
+})
 
+addPlayerOButton.addEventListener('click', () => {
+  playerNameO =  playerNameO.value.trim(); 
+  playerNameO.value = " ";
+  addPlayerOButton.disabled = true;
+})
+addPlayerXButton.addEventListener('click', () => {
+  playerNameX = playerNameX.value.trim(); 
+  playerNameX.value = " ";
+  addPlayerXButton.disabled = true;
+})
 
+// | - - - to start the game and timer - - - |
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
   start();
@@ -83,11 +113,11 @@ function winChecker () {
       }
     }
     if (xCount == 3) {
-      gameStatus.textContent = "Player X wins!";
+      gameStatus.textContent = playerNameX + " wins!";
       showWin(xLog);
       setTimeout(() => {location.reload(); }, 1000)
     } else if (oCount == 3) {
-      gameStatus.textContent = "Player O wins!";
+      gameStatus.textContent = playerNameO+ " wins!";
       setTimeout(() => {location.reload(); }, 1000);
       showWin(oLog);
     } else if (moveCount.length === 9) {
@@ -102,45 +132,34 @@ function showWin (winningArray) {
     document.getElementById('cell-' + winningArray[i]).style.background = "red"
   }
 }
-
+// | - - - place mark on board & change player - - - |
 function placeMark(numCell) {
   if (playerO) {
     document.getElementById('cell-' + numCell).textContent = "0";
     oLog.push(numCell);
     oLog.sort();
     moveCount.push(numCell);
-    console.log(oLog)
     playerO = false;
     gameStatus.textContent = "Player X's turn";
   }
   else if (playerO === false) {
     document.getElementById('cell-' + numCell).textContent = "X";
     xLog.push(numCell);
-    console.log(xLog)
     xLog.sort();
     moveCount.push(numCell);
     playerO = true;
     gameStatus.textContent = "Player O's turn";
   }
 }
-
+// | - - - game timer - - - |
 function timer() {
   setInterval(() => {countUp();}, 1000);
 }
-
 function countUp() {
   count = count + 1;
   document.getElementById('timer').textContent = count + " seconds have elapsed in play"
 }
 
-/*function counter() {
-  while (count <= 59) {
-  count = count + 1;
-  document.getElementById('timer').textContent = count + " seconds have elapsed in play"
-  }
-}
 
 
 
-//let board = Array.from(document.getElementsByClassName('board')) -- > for later
-//let eventListeners = () => board.forEach(childNodes => childNodes.addEventListener('click', placeMark) --> for later */
