6f4a9b078390deee27edd631c84b2d1bb8b1caf4
Lee Peters <lpeters0023@gmail.com>
2020-02-24 07:27:34 -0500

    fixed name functionality

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 5, 8], [0, 4, 8], [6, 4, 2]];
 let xLog = [];
 let oLog = [];
-let moveCount = []; 
-let gameStatus = document.getElementById('game-status');
+let moveCount = []; // for tracking location and number of moves
+let gameStatus = document.getElementById('game-status'); // display active player or win / draw
 let startButton = document.getElementById('start');
 let addPlayerOButton = document.getElementById('player-o-button')
 let addPlayerXButton = document.getElementById('player-x-button')
@@ -15,17 +15,19 @@ let singlePlayerButton = document.getElementById('single-player')
 let multiPlayerButton = document.getElementById('multi-player')
 let playerNameInput = document.getElementById('add-player-input')
     playerNameInput.style.visibility = "hidden"
-    
-// | - - - add player names - - - |
+
+// | - - - choose single or multi player - - - |
 singlePlayerButton.addEventListener('click', () => {
   multiPlayerButton.disabled = true;
-  //random num generator
+  addPlayerOButton.style.visibility = "visible"
+  playerNameO.style.visibility = "visible"
+  activateComputer();
 })
 multiPlayerButton.addEventListener('click', () => {
   singlePlayerButton.disabled = true;
   playerNameInput.style.visibility = "visible"
 })
-
+// | - - - add player names - - - |
 addPlayerOButton.addEventListener('click', () => {
   playerNameO =  playerNameO.value.trim(); 
   playerNameO.value = " ";
@@ -36,14 +38,13 @@ addPlayerXButton.addEventListener('click', () => {
   playerNameX.value = " ";
   addPlayerXButton.disabled = true;
 })
-
 // | - - - to start the game and timer - - - |
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
   start();
   timer();
 }); 
-
+// | - - - game function / unique cell event listeners - - - |
 function start() {
   document.getElementById('cell-0').addEventListener('click', () => {
     placeMark(0);
@@ -100,10 +101,10 @@ function start() {
   {once : true}
   );
 }
-
+// | - - - win check functions - - - |
 function winChecker () {
   for (let subCombo of winCombo) {
-    let xCount = 0;
+    let xCount = 0; // count of 3 signifies win 
     let oCount = 0;
     for (let num of subCombo) {
       if (xLog.includes(num)) {
@@ -126,7 +127,6 @@ function winChecker () {
     }
   }
 }
-
 function showWin (winningArray) {
   for (let i = 0; i < winningArray.length; i++) {
     document.getElementById('cell-' + winningArray[i]).style.background = "red"
@@ -139,16 +139,16 @@ function placeMark(numCell) {
     oLog.push(numCell);
     oLog.sort();
     moveCount.push(numCell);
-    playerO = false;
-    gameStatus.textContent = "Player X's turn";
+    playerO = false; // switch players
+    gameStatus.textContent = playerNameX + "'s turn";
   }
   else if (playerO === false) {
     document.getElementById('cell-' + numCell).textContent = "X";
     xLog.push(numCell);
     xLog.sort();
     moveCount.push(numCell);
-    playerO = true;
-    gameStatus.textContent = "Player O's turn";
+    playerO = true; // switch players
+    gameStatus.textContent = playerNameO + "'s turn";
   }
 }
 // | - - - game timer - - - |
