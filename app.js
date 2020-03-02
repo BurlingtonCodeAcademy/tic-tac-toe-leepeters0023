e0534f20cfe31b0d51bdf03eb8ce44e776951c3b
Lee Peters <lpeters0023@gmail.com>
2020-03-02 13:54:56 -0500

    yup

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public'))
 
 app.get('./index.html', (req, res) => {
-  res.sendFile(resolve('./public/index.html'))
+  res.sendFile(path.resolve('./public/index.html'))
 })
 
 // 1) git add + commit 2) heroku create 3) git push heroku master 
